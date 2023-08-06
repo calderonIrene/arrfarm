80462a8f40ed585979384605fa4d438c2577360f
Irene Calderón <calderonpochirene@gmail.com>
2023-08-06 18:33:36 +0200

    new changes on db_data_management

--- db_data_management.py
+++ db_data_management.py
@@ -1,139 +1,193 @@
 import mysql.connector
-import config as c
-
-# Configurem la info per conectarnos a la bd
-config = {
-    'user': c.db_user,
-    'password': c.db_password,
-    'host': 'localhost',
-    'database': 'arrfarm',
-    'raise_on_warnings': True
-}
-
-
-# Connecting to the database
-config = c.config
-cnx = mysql.connector.connect(**config)
-
+from config import config
+import os
+from datetime import date
 
 # SQL queries to access the database
-queryPrescription = "SELECT * FROM prescription WHERE state = 'demanat'" # Les comandes que s'han de preparar
-queryMedicines = "SELECT * FROM medicine" # Tots els medicaments
-queryMedicineOfPrescriptionX = "SELECT idMedicine, amount FROM medicine_prescription WHERE idPrescription = %s" # Medicament + quantitat demanada en la comanda X
-queryMedicineX = "SELECT name, stock FROM medicine WHERE idMedicine = %s" # Nom i stock de la medicina seleccionada
+#query_prescription requests for all the orders to be prepared
+query_prescription = "SELECT * FROM prescription WHERE state = 'demanat'" 
+#query_medicine_of_prescriptionX requests the id and stock of the medicines that are related to the prescription passed by parameter
+query_medicine_of_prescriptionX = "SELECT idMedicine, amount FROM medicine_prescription WHERE idPrescription = %s" 
+# query_medicineX requests the name and the stock of the medicine with the id passed by parameter
+query_medicineX = "SELECT name, stock FROM medicine WHERE idMedicine = %s" 
 
 # SQL queries to modify database
 queryToModifyStock =  "UPDATE medicine SET stock = %s WHERE idMedicine = %s"
 
 
-# Cursor will execute the SQL queries
-cursor = cnx.cursor()
+def make_query(query, param):
+    """
+    This function executes the requested query to the database
+
+    Args:
+        query (str): Query to be executed
+        param (any): Parameters passed to the query
+
+    Note:
+        The `query` argument must be a non-null string.
+
+    Returns:
+        The result of making the query passed by parameter to the database
+    """
 
 
-def fesConsulta(query, aux):
+    #cnx establishes a connection to the MySQL database using the provided configuration settings
     cnx = mysql.connector.connect(**config)
+    # cursor is going to execute queries on the cnx connection
     cursor = cnx.cursor()
     
     if query == "allPrescription":
-        cursor.execute(queryPrescription)
+        cursor.execute(query_prescription)
     
     elif query == "queryMedicineOfPrescriptionX":
-        cursor.execute(queryMedicineOfPrescriptionX, aux)
+        cursor.execute(query_medicine_of_prescriptionX, param)
     
     elif query == "medicineALaBD":
-        cursor.execute(queryMedicineX, aux)
+        cursor.execute(query_medicineX, param)
 
+    # results contains all the results of the query
     results = cursor.fetchall()
 
-    #Tanquem les conexions
+    # Connection is closed
     cursor.close()
     cnx.close()
 
     return results
 
-def actualitzaStock(param):
+def update_stock(param):
+    """
+    This function updates the stock of a product in the database.
+
+    Args:
+        param: Tuple containing the id of the product and the updated stock
+
+    Note:
+        Param must be a non-null tuple
+
+    Returns:
+        None
+    """
+
+    #cnx establishes a connection to the MySQL database using the provided configuration settings
     cnx = mysql.connector.connect(**config)
+    # cursor is going to execute queries on the cnx connection
     cursor = cnx.cursor()
 
+    # The query to modify the stock in the database is executed with the given parameters (param)
     cursor.execute(queryToModifyStock, param)
-    cnx.commit()  # Add this line to commit the changes to the database
+
+    # The changes to the database are commited
+    cnx.commit()  
     
-    #Tanquem les conexions
+    # Connection is closed
     cursor.close()
     cnx.close()
 
-def comandesAPreparar ():
+
+def orders_to_be_prepared ():
+    """
+    Returns a list containing all the orders to be prepared in the following format:
+
+        [[medicine 1, ...,  medicine n1], ..., [medicine 1, ...,  medicine nX]]
+
+        Each sublist represents the list of medicines for each order.
+
+        It also returns the list of medicines that the company needs to replenish.
+
+    Args:
+        None
+        
+    Returns:
+        list: A list of lists, where each sublist contains the medicines for each order.
+        list: A list of medicines that need to be replenished by the company.
+        
+    """
+
     orders=[]
     replenishmentList = []
 
-    allPrescriptions = fesConsulta("allPrescription",0)
+    allPrescriptions = make_query("allPrescription",None)
 
     for prescription in allPrescriptions:
         thisRecipieList = [prescription[0]]
 
-        # Creem l'albaran corresponent a la comanda 
-        #nameOfDeliveryNote = (str(prescription[0])+"_"+str(prescription[1])+".txt")
-        #deliveryNotefilepath = os.path.join("C:\\Users\\calde\\Desktop\\UNI\\TFG\\code\\albarans", str(nameOfDeliveryNote))
-        #deliveryNote = open(deliveryNotefilepath, "a")
-        #deliveryNote.write("COMANDA FETA PER: \n")
-        # deliveryNote.write("PREPARADA EL DIA: " + str(date.today()) +"\n")
+        name_of_delivery_note = (str(prescription[0])+"_"+str(prescription[1])+".txt")
 
-        #idsMedicinesOfPrescription guarda totes les id dels medicaments de la recepta
-        idsMedicinesOfPrescription = fesConsulta("queryMedicineOfPrescriptionX", (prescription[0],))
-
-        # idsMedicinesOfPrescription[0] -> code of medicine
-        # idsMedicinesOfPrescription[1] -> amount of medicine
+        delivery_note_file_path = os.path.join("C:\\Users\\calde\\Desktop\\UNI\\TFG\\src\\deliveryNotes", str(name_of_delivery_note))
+        print(delivery_note_file_path)
         
-        if idsMedicinesOfPrescription: # Si hi ha medicaments demanats
-            for medDemanada in idsMedicinesOfPrescription: # Per cada un dels medicaments demanats
+        output_folder = os.path.dirname(delivery_note_file_path)
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+        
+        delivery_note = open(delivery_note_file_path, "w")
 
-                # 1. medicineALaBD guardarà el medicament actual
-                medicineALaBD =  fesConsulta("medicineALaBD", (medDemanada[0],))
+        delivery_note.write("ORDER PLACED BY: \n")
+        delivery_note.write("PREPARED ON THE DAY: " + str(date.today()) +"\n")
 
-                print(medicineALaBD[0][1])
-                #print("Medicina demanada "+str(medDemanada))
-                #print("Medicina en la bd "+str(medicineALaBD))
+        # ids_medicines_of_prescription stores all the ids of the medicines in the prescription
+        ids_medicines_of_prescription = make_query("queryMedicineOfPrescriptionX", (prescription[0],))
+
+        # ids_medicines_of_prescription[0] -> code of medicine
+        # ids_medicines_of_prescription[1] -> amount of medicine
+        
+        # # if there are medicines ordered
+        if ids_medicines_of_prescription:  
+            # for each medicine ordered 
+            for med_ordered in ids_medicines_of_prescription:
+
+                # medicine_in_BD will save the current product
+                medicine_in_BD =  make_query("medicineALaBD", (med_ordered[0],))
+
+                print(medicine_in_BD[0][1])
+                print("Medicina demanada "+str(med_ordered))
+                print("Medicina en la bd "+str(medicine_in_BD))
                 
-                # Si hi ha stock de la medicina:         
-                if medicineALaBD[0][1] != 0:
-                    if medDemanada[1] <= medicineALaBD[0][1]: # Si podem servir el 100% de la quantitat de medicament demanada
-                        # Servirem indicarà la quantitat de medicament que servirem
-                        servirem = medDemanada[1] 
-                        # stockActual indicarà l'stock d'aquell medicament
-                        # amb el que ens quedarem despres de servir la comanda
-                        stockActual = medicineALaBD[0][1] - medDemanada[1]
+                # If the medicine is in stock:    
+                if medicine_in_BD[0][1] != 0:
+                    # If we can deliver the full amount of the medicine ordered:
+                    if med_ordered[1] <= medicine_in_BD[0][1]: 
+                        # delivered will store the amount of medication that will be served
+                        delivered = med_ordered[1] 
+                        # updated_stock will indicate the updated stock of that drug 
+                        # after the order has been served.
+                        updated_stock = medicine_in_BD[0][1] - med_ordered[1]
 
-                    else: # Si no tenim prou stock per servir al 100% la quantitat demanada
-                        servirem = medicineALaBD[0][1]
-                        stockActual = 0
+                    else: 
+                        # If we are unable to deliver the full amount of the medicine
+                        # The maximum number of products available will be served
+                        delivered = medicine_in_BD[0][1]
+                        updated_stock = 0
 
-                    if stockActual == 0:
-                        # Si ens hem quedat sense stock al servir la comanda, 
-                        # El posarem tambe a llista de medicaments a demanar
-                        replenishmentList.append(medicineALaBD[0][0])
+                    if updated_stock == 0:
+                        # If the remaining stock of the drug is 0, 
+                        # it will be noted in the list of drugs to be replenished
+                        replenishmentList.append(medicine_in_BD[0][0])
                             
-                    #  Actualizem albaran
-                    #deliveryNote.write("\nPRODUCT: " + str(medicineALaBD[0][0])+"")
-                    #deliveryNote.write("\nQUANTITAT DEMANADA: " + str(medDemanada[1]) + " --- QUANTITAT SERVIDA: " + str(servirem)+ "\n")
-                    #deliveryNote.write("\n Ara tenim un stock total de: " + str(stockActual)+ "\n")
-                        print("0")
+                    #  The delivery note of the order is updated with the information collected:
+                    delivery_note.write("\nPRODUCT: " + str(medicine_in_BD[0][0])+"")
+                    delivery_note.write("\nQUANTITAT DEMANADA: " + str(med_ordered[1]) + " --- QUANTITAT SERVIDA: " + str(delivered)+ "\n")
+                    delivery_note.write("\n Ara tenim un stock total de: " + str(updated_stock)+ "\n")
+                    print("0")
 
-                    #  Actualizem stock
-                    actualitzaStock((stockActual, medDemanada[0]))
+                    #  The stock of the drug in the database is updated
+                    update _stock((updated_stock, med_ordered[0]))
 
-                    #  Posem la medicina a la llista de la comanda
-                    if servirem >= 0:
-                        thisRecipieList.append(medicineALaBD[0][0])
-                        #thisRecipieList.append(medicineALaBD[0][1])
+                    # The medicine is added to the order list
+                    if delivered >= 0:
+                        thisRecipieList.append(medicine_in_BD[0][0])
+                        #thisRecipieList.append(medicine_in_BD[0][1])
 
-                
-                else: # Si no hi ha stock del medicament demanat
-                    #Ho indiquem a l'albaran
-                    replenishmentList.append(medicineALaBD[0][0])
-                    #deliveryNote.write("\nPRODUCT: " + str(medicineALaBD[0][0])+"")
-                    #deliveryNote.write("\nQUANTITAT DEMANADA: " + str(medDemanada[1]) + " --- QUANTITAT SERVIDA: 0\n")
+                # If the order could not be served because we do not have the medicine in stock:
+                else: 
+                    # It is added to the list of products to be replenished
+                    replenishmentList.append(medicine_in_BD[0][0])
+                    # It is indicated on the delivery note
+                    delivery_note.write("\nPRODUCT: " + str(medicine_in_BD[0][0])+"")
+                    delivery_note.write("\nQUANTITAT DEMANADA: " + str(med_ordered[1]) + " --- QUANTITAT SERVIDA: 0\n")
                                     
-        else: # No hi han medicaments demanats
+        # If there are no medicines ordered
+        else: 
             print("EMPTY")
         
         print(thisRecipieList)
@@ -141,4 +195,3 @@ def comandesAPreparar ():
         orders.append(thisRecipieList[1:])
 
     return orders, replenishmentList
-        
--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-import datetime
+mport datetime
 import db_data_management
-# import cam
 import alternative_cam as cam
 import client
-#import subprocess
 
-#subprocess.run(["python", "python_server.py"])
 
-groups =    [["A", "B", "C"], ["D","E","F"],["G","H","I"], 
-            ["J","K","L"], ["M", "N", "Ñ"], ["O", "P", "Q"], 
-            ["R", "S", "T"],["U", "V", "W"],["X", "Y", "Z"] ]
+# Grups will store the various groups into which the medicines are divided
+groups =    [["A", "B", "C","D","E","F","G","H","I"], 
+            ["J","K","L","M", "N", "Ñ","O", "P", "Q"], 
+            ["R", "S", "T","U", "V", "W","X", "Y", "Z"] ]
 
-# The file where the company's medicines to be refilled will be written is created
+
+# The file where the drugs to be replenished will be written is created.
 # The name of the file will be in the format day-month-year_HourMinute in which the order is being prepared
-fecha_hora_actual_str = datetime.datetime.now().strftime("%d-%m-%Y_%H%M")  
-replenishmentListfilepath = r"C:\Users\calde\Desktop\UNI\TFG\src\replenishmentFiles\{}.txt".format(fecha_hora_actual_str)
-replenishmentList = open(replenishmentListfilepath, "a")
+date_and_time = datetime.datetime.now().strftime("%d-%m-%Y_%H%M")  
+replenishment_list_filepath = r"C:\Users\calde\Desktop\UNI\TFG\src\replenishmentFiles\{}.txt".format(date_and_time)
+replenishment_list = open(replenishment_list_filepath, "a")
 
 # The list of commands to be prepared and the list of medicines that are no longer in stock are obtained
-orders, replenishment = db_data_management.comandesAPreparar()
+orders, replenishment = db_data_management.orders_to_be_prepared()
 
 # The file of the list of medicines that are no longer in stock is updated
 for med in replenishment:
-    replenishmentList.write("- " + med + "\n")
+    replenishment_list.write("- " + med + "\n")
 
 print(f"\replenishment:{replenishment}")           
 print(f"\norders:{orders}")        
+
+# The robot is notified that the commands preparation is starting
+client.send_message_nou_format("I;")
 
 numComanda = 0
 #1. FOR TASK IN TASKS:
@@ -33,30 +35,26 @@ for order in orders:
     
     numComanda+=1
     print(f"\nComanda {numComanda} en proces...")
-    # 2. FOR GROUP IN GROUPS:
+
+    #FOR GROUP IN GROUPS:
     for n, group in enumerate(groups):
         print(n)
-        #3. ST = SUBLIST OF TASK IN GROUP
+
+        # The medicines to be collected that belong to the group being processed are selected
         ST = [ELEMENT for ELEMENT in order if ELEMENT.startswith(tuple(group))]
-        # 4. WHILE LEN(ST) != 0:
+        
+        # If there are medicines of the group
         if len(ST) != 0:
+            # The group to be processed is sent to the robot
+            client.send_message_nou_format("C;%s;" % n)   
+
             for element in ST:
                 order.remove(element)
             print("Group "+str(group)+" "+str(ST))
+            
             #client.send_message("box_position",n) # The position of the present group container is sent to the robot
             client.send_message()
 
             cam.ferCalaix(ST) # Medicines from the current group are collected
 
-print("Totes les comandes ja han estat preparades! :)")
-
-'''
-
-        
-        
-            5. IF LECTURE IS IN ST:
-                CATCH MEDICINE (- REMOVE OF ST
-                                - TAKE PHYSICAL MEDICINE
-                                - BRING TO THE DROP-OFF SITE)
-            NEXT POSITION
-'''
+print("All orders have been prepared! :)")
