015ac39aeb98f4cae38a1510741f022441a7340c
Irene Calderón <calderonpochirene@gmial.com>
2023-08-26 10:59:44 +0200

    Main commented and cleaned

--- main.py
+++ main.py
@@ -10,34 +10,36 @@ groups =    [["A", "B", "C","D","E","F","G","H","I"],
             ["R", "S", "T","U", "V", "W","X", "Y", "Z"] ]
 
 
-# The file where the drugs to be replenished will be written is created.
+### The file where the drugs to be replenished will be written is created
+
 # The name of the file will be in the format day-month-year_HourMinute in which the order is being prepared
 date_and_time = datetime.datetime.now().strftime("%d-%m-%Y_%H%M")  
 replenishment_list_filepath = r"C:\Users\calde\Desktop\UNI\TFG\src\replenishmentFiles\{}.txt".format(date_and_time)
 replenishment_list = open(replenishment_list_filepath, "a")
 
-# The list of commands to be prepared and the list of medicines that are no longer in stock are obtained
+### The list of commands to be prepared and the list of medicines that are no longer in stock are obtained
 orders, replenishment = db_data_management.orders_to_be_prepared()
 
 # The file of the list of medicines that are no longer in stock is updated
 for med in replenishment:
     replenishment_list.write("- " + med + "\n")
 
-print(f"\replenishment:{replenishment}")           
-print(f"\norders:{orders}")        
+# Control messages
+#print(f"\replenishment:{replenishment}")           
+#print(f"\norders:{orders}")        
 
 # The robot is notified that the commands preparation is starting
 client.send_message("I;")
 
-numComanda = 0
+
+### The order preparation process begins.
+
 #1. FOR TASK IN TASKS:
-for order in orders:
-    
-    numComanda+=1
-    print(f"\nComanda {numComanda} en proces...")
+for orderNum, order in enumerate(orders):
+    print(f"\nOrder {orderNum} in process...")
 
     #FOR GROUP IN GROUPS:
-    for n, group in enumerate(groups):
+    for groupNum, group in enumerate(groups):
         #print(n)
 
         # The medicines to be collected that belong to the group being processed are selected
@@ -46,19 +48,20 @@ for order in orders:
         # If there are medicines of the group
         if len(ST) != 0:
             # The group to be processed is sent to the robot
-            client.send_message("C;%s;" % n)   
+            client.send_message("C;%s;" % groupNum)   
 
             for element in ST:
                 order.remove(element)
             print("Group "+str(group)+" "+str(ST))
             
-            #client.send_message("box_position",n) # The position of the present group container is sent to the robot
+            #client.send_message("box_position",groupNum) # The position of the present group container is sent to the robot
             #client.send_message()
 
-            time.sleep(30)
+            #time.sleep(30)
             #print("Dormo 30s")
 
             cam.ferCalaix(ST) # Medicines from the current group are collected
 
+            # The robot is notified that the commands preparation is finished
             #client.send_message("A;")
 print("All orders have been prepared! :)")
